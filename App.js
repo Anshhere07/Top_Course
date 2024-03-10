24abb28eaa7a60653c127069d8b933533b3d0a0f
Ansh Saxena <130739154+Anshhere07@users.noreply.github.com>
2024-03-10 20:52:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import "./App.css";
+import {apiUrl, filterData} from "./data";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import Spinner from "./components/Spinner";
+
+
+const App = () => {
+
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+  
+  async function fetchData() {
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      // output -> 
+      setCourses(output.data);
+    }
+    catch(error){
+      toast.error("Network me koi issue h");
+    }
+    setLoading(false);
+  }
+
+  useEffect( () => {
+    fetchData();
+  }, [])
+
+
+  return (
+    <div className="flex bg-slate-700 flex-col min-h-screen" >
+      <div>
+        <Navbar />
+      </div>
+
+
+      <div className="bg-slate-700">
+      <div>
+          <Filter 
+          filterData = {filterData}
+          category = {category}
+          setCategory = {setCategory}
+          />
+      </div>
+
+      <div className="w-11/12 max-w-[1200px] mx-auto
+      flex justify-center flex-wrap items-center min-h-[50vh]" >
+         {
+          loading ? (<Spinner />) : (<Cards courses={courses}
+          category = {category}/>)
+         }
+      </div> 
+      </div>
+   
+
+
+    </div>
+  );
+};
+
+export default App;
